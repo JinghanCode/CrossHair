1ac84b348056df7fa54f5310a5c2dd84e905e008
Matthew Jing Han Law <u6699654@anu.edu.au>
2021-03-25 19:31:19 +1100

    Pushing new tests

--- crosshair/examples/hypothesis/my_tests_failing.py
+++ crosshair/examples/hypothesis/my_tests_failing.py
@@ -11,9 +11,14 @@ def test_integers_minimal(x):
 def test_integers_basic(x):
     1 / x
 
+# This should fail with x in [1,5]
+@given(st.integers(1,5))
+def test_integers_bounded(x):
+    assert x == 10
+
 # This should fail with x = 117.
 @given(st.integers(0, 118))
-def test_integers_bounded(x):
+def test_integers_bounded_alternate(x):
     1 / (x - 117)
 
 # This should fail with x = 3,4,5
@@ -21,5 +26,9 @@ def test_integers_bounded(x):
 def zac_test(x):
     assert 1 <= x <= 2
 
+# This should fail with x = 5, y = -5
+@given(st.integers(min_value=1, max_value=5), st.integers(min_value=-100000))
+def test_multiple_strategies(x, y):
+     1 / (x + y)
 
 
--- crosshair/examples/hypothesis/my_tests_passing.py
+++ crosshair/examples/hypothesis/my_tests_passing.py
@@ -1,13 +1,16 @@
 import hypothesis.strategies as st
 from hypothesis import given
 
-# This test should pass.
 @given(st.integers(0, 116))
 def test_integers_bounded(x):
     1 / (x - 117)
 
-# This should pass.
+
 @given(st.integers(1,5))
 def zac_test(x):
     assert 1 <= x <= 5
+
+@given(st.integers(min_value=1, max_value=5), st.integers(min_value=-100000, max_value=-1000))
+def test_multiple_strategies(x, y):
+    1 / (x + y)
 
